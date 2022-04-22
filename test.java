4e50df0cb0a2b5bc2a2c362ac932e5d3cf613e10
prarkan <102149906+prarkan@users.noreply.github.com>
2022-04-22 11:52:39 -0700

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,13 @@
 public static void main(String [] args){
 
 
-System.out.println("Hello World!")
+System.out.println("Hello World!");
+  
+  for(int x =0; x<10 ; x++){
+    System.out.println(x):
+  
+  
+  }
 
 
 }
